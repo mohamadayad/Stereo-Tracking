e8960761e254165ccaa9a08c03eb0a0ab1061933
Mohayad <mohamad.ayad@tum.de>
2019-08-22 02:37:42 +0700

    Add LED marker detection

---
+++ src/LEDDetection.cpp
@@ -0,0 +1,190 @@
+
+
+#include <iostream>
+
+#include <boost/thread/thread.hpp>
+#include <pcl/common/common_headers.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+using namespace std;
+// --------------
+// -----Help-----
+// --------------
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+  return (viewer);
+}
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> customColourVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(cloud, 0, 255, 0);
+  viewer->addPointCloud<pcl::PointXYZ> (cloud, single_color, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+  return (viewer);
+}
+
+unsigned int text_id = 0;
+void keyboardEventOccurred (const pcl::visualization::KeyboardEvent &event,
+                            void* viewer_void)
+{
+  pcl::visualization::PCLVisualizer *viewer = static_cast<pcl::visualization::PCLVisualizer *> (viewer_void);
+  if (event.getKeySym () == "r" && event.keyDown ())
+  {
+    std::cout << "r was pressed => removing all text" << std::endl;
+
+    char str[512];
+    for (unsigned int i = 0; i < text_id; ++i)
+    {
+      sprintf (str, "text#%03d", i);
+      viewer->removeShape (str);
+    }
+    text_id = 0;
+  }
+}
+
+void mouseEventOccurred (const pcl::visualization::MouseEvent &event,
+                         void* viewer_void)
+{
+  pcl::visualization::PCLVisualizer *viewer = static_cast<pcl::visualization::PCLVisualizer *> (viewer_void);
+  if (event.getButton () == pcl::visualization::MouseEvent::LeftButton &&
+      event.getType () == pcl::visualization::MouseEvent::MouseButtonRelease)
+  {
+    std::cout << "Left mouse button released at position (" << event.getX () << ", " << event.getY () << ")" << std::endl;
+
+    char str[512];
+    sprintf (str, "text#%03d", text_id ++);
+    viewer->addText ("clicked here", event.getX (), event.getY (), str);
+  }
+}
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> interactionCustomizationVis ()
+{
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  viewer->addCoordinateSystem (1.0);
+
+  viewer->registerKeyboardCallback (keyboardEventOccurred, (void*)viewer.get ());
+  viewer->registerMouseCallback (mouseEventOccurred, (void*)viewer.get ());
+
+  return (viewer);
+}
+
+
+
+int main (int argc, char** argv)
+{
+
+    bool simple = true;
+    std::cout << "Simple visualisation example\n";
+
+
+
+  // ------------------------------------
+  // -----Create example point cloud-----
+  // ------------------------------------
+  pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
+  std::cout << "Genarating example point clouds.\n\n";
+  // We're going to make an ellipse extruded along the z-axis. The colour for
+  // the XYZRGB cloud will gradually go from red to green to blue.
+  uint8_t r(255), g(15), b(15);
+  for (float z(-1.0); z <= 1.0; z += 0.05)
+  {
+    for (float angle(0.0); angle <= 10.0; angle += 1.0)
+    {
+      pcl::PointXYZ basic_point;
+      basic_point.x = angle;
+      basic_point.y = angle;
+      basic_point.z = angle;
+      basic_cloud_ptr->points.push_back(basic_point);
+
+      pcl::PointXYZRGB point;
+      point.x = basic_point.x;
+      point.y = basic_point.y;
+      point.z = basic_point.z;
+      uint32_t rgb = (static_cast<uint32_t>(r) << 12 |
+              static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
+      point.rgb = *reinterpret_cast<float*>(&rgb);
+
+      point_cloud_ptr->points.push_back (point);
+    }
+    if (z < 0.0)
+    {
+      r -= 12;
+      g += 12;
+    }
+    else
+    {
+      g -= 12;
+      b += 12;
+    }
+  }
+  basic_cloud_ptr->width = (int) basic_cloud_ptr->points.size ();
+  basic_cloud_ptr->height = 1;
+  point_cloud_ptr->width = (int) point_cloud_ptr->points.size ();
+  point_cloud_ptr->height = 1;
+
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(basic_cloud_ptr, 0, 255, 0);
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+  if (simple)
+  {
+    viewer = customColourVis(basic_cloud_ptr);
+  }
+
+  //--------------------
+  // -----Main loop-----
+  //--------------------
+  float count=0.0;
+  bool linehere=false;
+  while (!viewer->wasStopped ())
+  {
+
+      if(count >-10){
+      count+=-1;
+
+      pcl::PointXYZ basic_point;
+      basic_point.x = count;
+      basic_point.y = count;
+      basic_point.z = count;
+      basic_cloud_ptr->points.push_back(basic_point);
+//      viewer->removeAllPointClouds();
+//      viewer->addPointCloud(basic_cloud_ptr);
+
+      if(!linehere){
+          pcl::PointXYZ p1, p2;
+          p1.x=0.0; p1.y=0.0; p1.z=0.0;
+          p2.x=5.0; p2.y=5.0; p2.z=5.0;
+          viewer->addLine(p1, p2, "line1", 0);
+          linehere=true;
+      }
+  }
+
+      viewer->updatePointCloud(basic_cloud_ptr,single_color,"sample cloud");
+
+      viewer->spinOnce (10);
+
+    boost::this_thread::sleep (boost::posix_time::microseconds (1000000));
+  }
+  std::cout << "jeeba" << std::endl;
+
+  return EXIT_SUCCESS;
+}
